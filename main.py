339e9782c04cb7cdbf6a6225c7447b98e5f9435f
Sanika Shahane <141646903+sanika83@users.noreply.github.com>
2026-06-08 00:47:47 +0530

    Add main.py and project structure

---
+++ main.py
@@ -0,0 +1,11 @@
+autonomous-drone/
+├── README.md
+├── main.py
+├── requirements.txt
+├── docs/
+│   └── project_report.pdf
+├── images/
+│   ├── drone_design.jpg
+│   ├── before_obstacle.jpg
+│   └── after_obstacle.jpg
+└── .gitignore
